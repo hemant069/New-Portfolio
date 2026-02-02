07877c341c7abf97e4e94e8312dd6d8a9e765441
hemant069 <hemantprajapati20500@gmail.com>
2026-02-02 19:47:34 +0530

    added the github calender

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,55 +1,24 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
-import Image from "next/image";
-import Skills from "./Skills";
 import Projects from "./Projects";
 import { Contact } from "./Contact";
-import Social from "./Social";
-import Header from "./Header";
 import Hero from "./Hero";
 import WorkExperience from "./WorkExperience";
-import Test from "./Test";
+import Github from "./Github";
+
 
 const Main = () => {
 
 
   return (
-    <>
+    <div>
 
-      <div>
-        <Hero />
-      </div>
-      <div>
-        <WorkExperience />
-      </div>
-      <div >
-        <Projects />
-      </div>
+      <Hero />
+      <WorkExperience />
+      <Projects />
+      <Github />
+      <Contact />
 
-      <div
-
-        className=""
-      >
-        <Contact />
-      </div>
-      <div>
-
-      </div>
-      {/* <div>
-        <h2 className="text-2xl font-bold text-center mb-6"></h2>
-        <div className="flex justify-center mt-20">
-          <Skills />
-        </div>
-      </div>
-
-      <div ref={inputRef} className="mt-20 sm:px-14">
-        <Projects />
-      </div>
-     
-      <div className="flex justify-center mt-5">
-        <Social />
-      </div> */}
-    </>
+    </div>
   );
 };
 
