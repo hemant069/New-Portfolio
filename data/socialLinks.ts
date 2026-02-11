a0a7fb4bf1052cc9f76f852e2adec4f7befd0792
hemant069 <hemantprajapati20500@gmail.com>
2026-02-11 17:42:13 +0530

    added the resume on icons

--- data/socialLinks.ts
+++ data/socialLinks.ts
@@ -1,12 +1,20 @@
 import { SocialLink } from "@/types/socialTypes";
-import { Github, Instagram, Linkedin, Mail, Twitter } from "lucide-react";
+import { Github, Instagram, Linkedin, Mail, Twitter, NotebookTabs } from "lucide-react";
 import githubPreview from "../public/github-preview.webp";
 import linkedinPreview from "../public/linkedin-preview.webp";
 import twitterPreview from "../public/twitter-preview.webp";
 import instagramPreview from "../public/instagram.png";
 import MailPreview from "../public/mailpreview.png";
+import Resume from "../public/resume.png"
 
 export const socialLinks: SocialLink[] = [
+  {
+    icon: NotebookTabs,
+    label: "Resume",
+    href: "https://drive.google.com/file/d/1-_jmtYaXVYKAyEKoQSmhLvoiU4kjqBTx/view?usp=drive_link",
+    previewImg: Resume,
+  }
+  ,
   {
     icon: Github,
     label: "GitHub",
@@ -37,4 +45,5 @@ export const socialLinks: SocialLink[] = [
     href: "https://www.instagram.com/hemant_069/",
     previewImg: instagramPreview,
   },
+
 ];
