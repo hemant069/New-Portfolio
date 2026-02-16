9000d94b8cc4587e7d3b8073fd92125e5dd38014
hemant069 <hemantprajapati20500@gmail.com>
2026-02-16 20:27:47 +0530

    revamping the folder for the cal

---
+++ config/calconfig.ts
@@ -0,0 +1,9 @@
+export const calconfig={
+    namespace:"30min",
+    link:"hemant-prajapati-dogwqr/30min",
+    config:{
+        layout:"month_view",
+        useSlotsViewOnSmallScreen:true,
+        theme:"dark"
+    }
+}
